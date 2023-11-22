38b29c1c12086ee7de1668e79dba9b9f15580334
baggyeonghyeon <rudgus4620@gmail.com>
2023-11-22 12:48:12 +0900

    login-dice

--- lib/dice.dart
+++ lib/dice.dart
@@ -2,10 +2,55 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Dice extends StatelessWidget {
+import 'dart:math';
+
+class Dice extends StatefulWidget {
+
+  @override
+  State<Dice> createState() => _DiceState();
+}
+
+class _DiceState extends State<Dice> {
+  int leftDice = 1;
+  int rightDice = 2;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.redAccent,
+      appBar: AppBar(
+        backgroundColor: Colors.redAccent,
+        title: const Text('Dice game'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    // flex: 1, // 비율임 flex가 커지면 큼
+                      child: Image.asset('image/dice$leftDice.png')),
+                  const SizedBox(width: 20.0,),
+                  Expanded(
+                      child: Image.asset('image/dice$rightDice.png')),
+                ],
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    leftDice = Random().nextInt(6) + 1;
+                    rightDice = Random().nextInt(6) + 1;
+                  });
+                },
+                child: const Icon(Icons.arrow_circle_down)),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -48,99 +48,108 @@ class _LogInState extends State<LogIn> {
         ],
       ),
       body: Builder(
+
         builder: (context) {
-          return SingleChildScrollView(
-            child: Column(
-              children: [
-                const Padding(
-                  padding: EdgeInsets.only(top: 50),
-                  child: Center(
-                    child: Image(
-                      image: AssetImage('image/chef.gif'),
-                      width: 170.0,
-                      height: 190.0,
-                    ),
-                  ),
-                ),
-                Form(
-                  child: Theme(
-                    data: ThemeData(
-                      primaryColor: Colors.teal, // 텍스트 필드 눌렀을때
-                      inputDecorationTheme: const InputDecorationTheme(
-                        labelStyle: TextStyle(color: Colors.teal,
-                            fontSize: 15.0),
-                      ),
-                    ),
-                    child: Container(
-                      padding: const EdgeInsets.all(40.0),
-                      child: Column(
-                        children: [
-                          TextField(
-                            controller: controller,
-                            decoration: InputDecoration(
-                              labelText: "Enter dice",
-                            ),
-                            keyboardType: TextInputType.emailAddress,
-                          ),
-                          TextField(
-                            controller: controller2,
-                            obscureText: true,
-                            decoration: InputDecoration(
-                              labelText: "Enter password",
-                            ),
-                            keyboardType: TextInputType.text,
-                          ),
-                          const SizedBox(height: 40.0,),
-                          ButtonTheme(
-                            minWidth: 100.0,
-                            height: 50.0,
-                            child: ElevatedButton(
-                              child: const Icon(
-                                Icons.arrow_forward,
-                                size: 35.0,
-                                color: Colors.orangeAccent,),
-                              onPressed: () {
-                                if (controller.text == 'dice' &&
-                                    controller2.text == '1234') {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (BuildContext context) =>
-                                              Dice()));
-                                }
-                                else if (controller.text != 'dice' &&
-                                    controller2.text == '1234') {
-                                  ScaffoldMessenger.of(context).showSnackBar(
-                                    const SnackBar(content: Text('아이디 입력 이상'),
-                                      duration: Duration(seconds: 3),
-                                    ),
-                                  );
-                                }
-                                else if (controller.text == 'dice' &&
-                                    controller2.text != '1234') {
-                                  ScaffoldMessenger.of(context).showSnackBar(
-                                   const  SnackBar(content: Text('비번 입력 이상'),
-                                      duration: Duration(seconds: 3),
-                                    ),
-                                  );
-
-                                } else {
-                                  ScaffoldMessenger.of(context).showSnackBar(
-                                    const SnackBar(content: Text('다시 입력하세요'),
-                                      duration: Duration(seconds: 3),
-                                    ),
-                                  );
-
-                                }
-                              },
-                            ),
-                          ),
-                        ],
+          return GestureDetector(
+            onTap: () { // onPressed는 버튼 눌렀을때  onTap은 그거 + 포괄적인거 두번 누르기 길게 누르기 등
+              // FocusNode: 포커르를 받는 특정 위젯을 식별함
+              // FocusScope : 어떤 위젯들까지 포커스를 받는지 범위를 나타냄
+              FocusScope.of(context).unfocus(); // 이러면 빈공간 입력시 키보드 사라짐!!
+            },
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(top: 50),
+                    child: Center(
+                      child: Image(
+                        image: AssetImage('image/chef.gif'),
+                        width: 170.0,
+                        height: 190.0,
                       ),
                     ),
                   ),
-                ),
-              ],
+                  Form(
+                    child: Theme(
+                      data: ThemeData(
+                        primaryColor: Colors.teal, // 텍스트 필드 눌렀을때
+                        inputDecorationTheme: const InputDecorationTheme(
+                          labelStyle: TextStyle(color: Colors.teal,
+                              fontSize: 15.0),
+                        ),
+                      ),
+                      child: Container(
+                        padding: const EdgeInsets.all(40.0),
+                        child: Column(
+                          children: [
+                            TextField(
+                              controller: controller,
+                              decoration: InputDecoration(
+                                labelText: "Enter dice",
+                              ),
+                              keyboardType: TextInputType.emailAddress,
+                            ),
+                            TextField(
+                              autofocus: true, // 뭔지 적기
+                              controller: controller2,
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                labelText: "Enter password",
+                              ),
+                              keyboardType: TextInputType.text,
+                            ),
+                            const SizedBox(height: 40.0,),
+                            ButtonTheme(
+                              minWidth: 100.0,
+                              height: 50.0,
+                              child: ElevatedButton(
+                                child: const Icon(
+                                  Icons.arrow_forward,
+                                  size: 35.0,
+                                  color: Colors.orangeAccent,),
+                                onPressed: () {
+                                  if (controller.text == 'dice' &&
+                                      controller2.text == '1234') {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (BuildContext context) =>
+                                                Dice()));
+                                  }
+                                  else if (controller.text != 'dice' &&
+                                      controller2.text == '1234') {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(content: Text('아이디 입력 이상'),
+                                        duration: Duration(seconds: 3),
+                                      ),
+                                    );
+                                  }
+                                  else if (controller.text == 'dice' &&
+                                      controller2.text != '1234') {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                     const  SnackBar(content: Text('비번 입력 이상'),
+                                        duration: Duration(seconds: 3),
+                                      ),
+                                    );
+
+                                  } else {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(content: Text('다시 입력하세요'),
+                                        duration: Duration(seconds: 3),
+                                      ),
+                                    );
+
+                                  }
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           );
         }
